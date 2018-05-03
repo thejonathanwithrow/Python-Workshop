68f0ea8da596b6885f903f5c667be22e4cd452db
thejonathanwithrow <37046949+thejonathanwithrow@users.noreply.github.com>
2018-05-02 20:45:57 -0400

    bug2fix

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -16,12 +16,11 @@ def subtract_num(num1, num2):
     return num1 - num2
 
 
-
 def multiply_num(num1, num2):
     return num1 * num2
 
 
-def divide_num():
+def divide_num(num1, num2):
     return num1 / num2
 
 
